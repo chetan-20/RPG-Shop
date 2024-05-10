0b097decc4e7f6a480bef140fe1f2f3958faa653
Chetan Rautela <chetanrautela20@gmail.com>
2024-05-10 15:06:59 +0530

    added hovering script

--- Assets/Scripts/ShopScripts/ShopItemsSO.cs
+++ Assets/Scripts/ShopScripts/ShopItemsSO.cs
@@ -21,8 +21,7 @@ public class ShopItemsSO : ScriptableObject
         Legendary
     }
     private enum type
-    {
-        None,
+    {       
         Material,
         Weapon,
         Consumable,
