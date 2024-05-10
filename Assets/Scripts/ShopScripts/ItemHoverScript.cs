1d87639f806ed85bda0d912740ca9b095109f603
Chetan Rautela <chetanrautela20@gmail.com>
2024-05-10 18:11:22 +0530

    shows description on hover

--- Assets/Scripts/ShopScripts/ItemHoverScript.cs
+++ Assets/Scripts/ShopScripts/ItemHoverScript.cs
@@ -1,27 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class ItemHoverScript : MonoBehaviour
+using UnityEngine.EventSystems;
+public class ItemHoverScript : MonoBehaviour,IPointerEnterHandler,IPointerExitHandler
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] GameObject itemDescriptionPanel;
+    [SerializeField] private Transform targetElement;
+    private void Start()
     {
-        
+        itemDescriptionPanel.SetActive(false);
+    }
+    public void OnPointerEnter(PointerEventData eventData)
+    {        
+       itemDescriptionPanel.SetActive(true);
+        if (targetElement != null)
+        {
+            SetPanelInFrontOfTarget();
+        }
+        else
+        {
+            Debug.Log("Missing Next Items Rect Transform for ItemDescription Panel");
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnPointerExit(PointerEventData eventData)
     {
-        
+        itemDescriptionPanel?.SetActive(false);
     }
-
-    private void ShowDescription()
+    public void SetPanelInFrontOfTarget()
     {
-
-    }
-    private void HideDescription()
-    {
-
+        itemDescriptionPanel.transform.SetParent(targetElement.parent); // Set the panel's parent to the same parent as the target element
+        itemDescriptionPanel.transform.SetAsLastSibling(); // Make sure the panel is rendered last in the hierarchy
+       
     }
 }
