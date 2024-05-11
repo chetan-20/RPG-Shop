8e796e87d513bf631ee499eb3c8278a361891195
Chetan Rautela <chetanrautela20@gmail.com>
2024-05-11 14:30:37 +0530

    created Item Template to keep reference for the ItemUI elements and made item prefab

---
+++ Assets/Scripts/ShopScripts/ItemsTemplate.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+public class ItemsTemplate : MonoBehaviour
+{
+    [SerializeField] private Button buyButton;
+    [SerializeField] private Image iconImage;
+    [SerializeField] private TMP_Text priceText;
+    [SerializeField] private TMP_Text QuantityText;
+    [SerializeField] private TMP_Text descriptionText;
+    [SerializeField] private TMP_Text rarityText;
+    [SerializeField] private TMP_Text itemweightText;
+
+}
